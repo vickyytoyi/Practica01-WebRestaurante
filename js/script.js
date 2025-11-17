ef1452a9922c3c5829ed6d4d8599ec5976a8bf4a
vickyytoyi <victoriapaz.ruiztagle@alumnos.udit.es>
2025-11-17 21:15:06 +0100

    galeria menu Scroll Reveal JS
    Homepage

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log("Hello world!");
+
+// SCROLL REVEAL JS
+ScrollReveal().reveal(".grid-wrapper > div", {
+    delay: 200,
+    distance: "50px",
+    interval: 100,
+    origin: "bottom",
+    scale: 0.9,
+});
